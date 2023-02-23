822d5f50b6bd826daec5fab56367c5b638b50fda
Ирина Капкова <kapiral@rambler.ru>
2023-02-23 22:00:55 +0400

    Initial commit

---
+++ src/test/java/me/ikapkova/recipewebsite/RecipeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ikapkova.recipewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
